c74fb5c24ecd7da9912664ceef96da63e47e005a
Yuki-Hub98 <wellyson.yago1@gmail.com>
2023-11-22 20:06:04 -0400

    Subindo o projeto

---
+++ src/js/data.js
@@ -0,0 +1,21 @@
+var lista = {
+    
+    verduras: {
+    "id": 1,
+    "nome": "Tomate",
+    "preco": 5.00
+        },
+    frutas: {
+
+    },
+    frios: {
+
+    },
+    mercearia: {
+
+    },
+    higiene:{
+
+    }}
+
+export {lista};
