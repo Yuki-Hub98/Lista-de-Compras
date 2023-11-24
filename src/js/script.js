079eaeaa5630e606b6625fe135b02964148f3f89
Daniel Batista <daniel_batistasj@hotmail.com>
2023-11-24 04:22:56 -0300

    Transformando uma página estatica em uma página dinâmica - Editar - Excluir - Adicionar - Renderizar

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,78 @@
-document.getElementById('add').addEventListener('click', function () {
-  addItemToList();
-});
+produtos =  []
+let preco = []
+const postDB = (lista_compras) => {
+  localStorage.setItem("lista-de-compras", JSON.stringify(lista_compras))
+  return lista_compras
+}
 
-function addItemToList() {
-  var newItemText = document.getElementById('item').value;
+const getDB = () => {
+  let lista_compras = JSON.parse(localStorage.getItem('lista-de-compras') ?? '[]');
+  return lista_compras
+}
 
-  if (newItemText.trim() !== '') {
-    var itemList = document.getElementById('itemList');
+const adcionar = (nome, preco) => {
+  let DB = getDB();
+  for (let i = 0; i < 1 ; i++) {
+      DB.push({"id": produtos.length, "nome": nome, "preco":preco})
+      postDB(DB);        
+   }
+}
 
+const excluir = (item) => {
+  let itensRemove = getDB().filter((produtos) => produtos.nome !== item)
+  postDB(itensRemove)
+}
+
+const editar = (item, itemEditado, moneyEdite, itemValue) => {
+  let array = getDB().map((produtos) => {
+      if (produtos.nome === item && Number(produtos.preco) === moneyEdite) {
+          produtos.nome = itemEditado
+          produtos.preco = itemValue
+      }
+      return produtos
+  })
+  
+  postDB(array);
+}
+
+const render = () =>{
+
+ 
+  let DB = getDB().map((element) => {
+
+    let itemList = document.getElementById('itemList');
+    let id = "Produto" + element.length
+    let idMoney = "Money" + element.length
     var newItem = document.createElement('li');
     newItem.className = 'item';
 
-    var itemContent = document.createElement('div');
+    let itemContent = document.createElement('div');
     itemContent.className = 'item-content';
-    itemContent.innerHTML = '<i class="fa-solid fa-wallet" style="color: #fff;"></i>' +
-      '<p class="itens">' + newItemText + '</p>';
+    itemContent.innerHTML = `<i class="fa-solid fa-wallet" style="color: #fff;"></i>
+    <p id=${id} class="itens">` + element.nome + '</p>';
+    
 
-    var moneyElement = document.createElement('p');
+    let moneyElement = document.createElement('p');
     moneyElement.className = 'money';
-    moneyElement.textContent = 'R$0,00';
+    moneyElement.id = idMoney
+    moneyElement.textContent = element.preco;
 
-    var itemActions = document.createElement('div');
+    let itemActions = document.createElement('div');
     itemActions.className = 'item-actions';
     itemActions.innerHTML = '<i class="fa-solid fa-pen-to-square" style="color: #00a81c;"></i>' +
       '<i class="fa-solid fa-trash" style="color: #d60000;"></i>';
 
-    // Adiciona a função de exclusão ao ícone de lixeira
     itemActions.querySelector('.fa-trash').addEventListener('click', function () {
+      let item = document.getElementById(id).textContent
+      excluir(item);
       deleteItem(newItem);
     });
 
-    // Adiciona a função de edição ao ícone de edição
     itemActions.querySelector('.fa-pen-to-square').addEventListener('click', function () {
-      editItem(itemContent);
+      let itemEditar = document.getElementById(id).textContent
+      let moneyEdite = Number(document.getElementById(idMoney).textContent)
+     
+      editItem(itemContent, itemEditar, moneyEdite);
     });
 
     newItem.appendChild(itemContent);
@@ -41,7 +81,66 @@ function addItemToList() {
 
     itemList.appendChild(newItem);
 
-    // Limpa o campo de entrada após adicionar o item
+
+    document.getElementById('item').value = '';
+  })
+  
+  return DB;
+}
+render()
+
+document.getElementById('add').addEventListener('click', function () {
+  addItemToList();
+  
+});
+
+function addItemToList() {
+  var newItemText = document.getElementById('item').value;
+  
+  if (newItemText.trim() !== '') {
+   
+    var itemList = document.getElementById('itemList');
+    let id = "Produto" + getDB().length
+    let idMoney = "Money" + getDB().length
+    var newItem = document.createElement('li');
+    newItem.className = 'item';
+
+    var itemContent = document.createElement('div');
+    itemContent.className = 'item-content';
+    itemContent.innerHTML = `<i class="fa-solid fa-wallet" style="color: #fff;"></i>
+    <p id=${id} class="itens">` + newItemText + '</p>';
+    
+
+    var moneyElement = document.createElement('p');
+    moneyElement.className = 'money';
+    moneyElement.id = idMoney
+    moneyElement.textContent = 0.00;
+
+    var itemActions = document.createElement('div');
+    itemActions.className = 'item-actions';
+    itemActions.innerHTML = '<i class="fa-solid fa-pen-to-square" style="color: #00a81c;"></i>' +
+      '<i class="fa-solid fa-trash" style="color: #d60000;"></i>';
+    
+    adcionar(newItemText, moneyElement.textContent)
+
+    itemActions.querySelector('.fa-trash').addEventListener('click', function () {
+      let item = document.getElementById(id).textContent
+      excluir(item);
+      deleteItem(newItem);
+    });
+
+    itemActions.querySelector('.fa-pen-to-square').addEventListener('click', function () {
+      let itemEditar = document.getElementById(id).textContent
+      let moneyEdite = Number(document.getElementById(idMoney).textContent)
+     
+      editItem(itemContent, itemEditar, moneyEdite);
+    });
+
+    newItem.appendChild(itemContent);
+    newItem.appendChild(moneyElement);
+    newItem.appendChild(itemActions);
+    itemList.appendChild(newItem);
+
     document.getElementById('item').value = '';
   }
 }
@@ -51,10 +150,13 @@ function deleteItem(item) {
   itemList.removeChild(item);
 }
 
-function editItem(itemContent) {
+function editItem(itemContent, itemEditar, moneyEdite ) {
   var itemName = prompt('Editar Nome:', itemContent.querySelector('.itens').textContent);
-  var itemValue = prompt('Editar Valor:', itemContent.nextElementSibling.textContent);
-
+  var itemValue = Number(prompt('Editar Valor:', itemContent.nextElementSibling.textContent));  
+  /*preco.push(itemValue)
+  console.log(preco)*/
+  editar(itemEditar, itemName, moneyEdite, itemValue)
+  
   if (itemName !== null && itemName.trim() !== '') {
     itemContent.querySelector('.itens').textContent = itemName;
     itemContent.nextElementSibling.textContent = itemValue || 'R$0,00';
